30ef6999b4955520e3ce14900d17038f2e6a14ce
f4789334 <manmay.e.mohanty@gmail.com>
2017-06-13 17:37:05 +0200

    AMQP in memory

--- src/main/java/za/co/reverside/service/Application.java
+++ src/main/java/za/co/reverside/service/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 
 import com.zenerick.service.notification.Service;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,9 +24,23 @@ public class Application {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("Notification API")
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.zenerick.service.notification"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(new ApiInfoBuilder()
+				.title("Reverside Notification API")
+				.description("API for notifications through different channels.")
+				.contact(new Contact(
+					"Reverside Innovation Lab",
+					"http://reverside.co.za/innovations-lab",
+					"manmay.moahnty@reverside.co.za"))
+				.license("License")
+				.licenseUrl("http://reverside.co.za/license")
+				.version("0.0.1")
+				.build());
 	}
+
+
 }
