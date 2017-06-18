38043bec5aca6d7d99cd8ee784e52eb7f7121198
Manmay <manmay.e.mohanty@gmail.com>
2017-06-18 19:47:06 +0200

    Added api.json

--- src/main/java/za/co/reverside/service/Application.java
+++ src/main/java/za/co/reverside/service/Application.java
@@ -3,44 +3,12 @@ package za.co.reverside.service;
 import org.springframework.boot.SpringApplication;
 
 import com.zenerick.service.notification.Service;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Service
-@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("Notification API")
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.zenerick"))
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(new ApiInfoBuilder()
-				.title("Reverside Notification API")
-				.description("API for notifications through different channels.")
-				.contact(new Contact(
-					"Reverside Innovation Lab",
-					"http://reverside.co.za/innovation-lab",
-					"manmay.mohanty@reverside.co.za"))
-				.license("License")
-				.licenseUrl("http://reverside.co.za/license")
-				.version("0.0.1")
-				.build());
-	}
-
 
 }
